3d3c0aa864f3fb75fdb14875e091d70633dfd04e
Aries Chow <arieschow1234a@gmail.com>
2014-05-05 18:47:10 +0800

    Initial Commit

---
+++ Bookwalker/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Bookwalker
+//
+//  Created by Aries on 5/5/14.
+//  Copyright (c) 2014年 Aries. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ BookwalkerTests/BookwalkerTests.m
@@ -0,0 +1,34 @@
+//
+//  BookwalkerTests.m
+//  BookwalkerTests
+//
+//  Created by Aries on 5/5/14.
+//  Copyright (c) 2014年 Aries. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BookwalkerTests : XCTestCase
+
+@end
+
+@implementation BookwalkerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end

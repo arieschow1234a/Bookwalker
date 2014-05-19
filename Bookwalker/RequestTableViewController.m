d366a55ec00342cd986e29514104162ad2df45da
Aries Chow <arieschow1234a@gmail.com>
2014-05-19 14:43:31 +0800

    Build 2 types of Requests
    Retrieved my requests and requests from others.
    Tableview split into 2 sections for each type of requests.

--- Bookwalker/RequestTableViewController.m
+++ Bookwalker/RequestTableViewController.m
@@ -11,18 +11,57 @@
 #import <Parse/Parse.h>
 
 @interface RequestTableViewController ()
-@property (strong, nonatomic) NSArray *requests;
+@property (strong, nonatomic) NSMutableArray *allRequests;
+@property (strong, nonatomic) NSMutableArray *myRequests;
+@property (strong, nonatomic) NSMutableArray *requestsFromOthers;
 @end
 
 @implementation RequestTableViewController
 
-- (NSArray *)requests
+- (NSMutableArray *)allRequests
 {
-    if(!_requests){
-        _requests = [[NSArray alloc] init];
-    }
-    return _requests;
+    if (!_allRequests) {
         
+      //  self.myRequests = [[NSMutableArray alloc] init];
+      //  self.requestsFromOthers = [[NSMutableArray alloc] init];
+        _allRequests = [[NSMutableArray alloc] init];
+    }
+    return _allRequests;
+}
+
+- (void)setMyRequests:(NSMutableArray *)myRequests
+{
+    _myRequests = myRequests;
+    if (![self.allRequests count]) {
+        [self.allRequests addObject:myRequests];
+    }else if ([self.requestsFromOthers count] && [self.allRequests count] == 1){
+        [self.allRequests insertObject:myRequests atIndex:0];
+    }else if ([self.allRequests count] == 2){
+        [self.allRequests replaceObjectAtIndex:0 withObject:myRequests];
+    }else{
+        [self.allRequests replaceObjectAtIndex:0 withObject:myRequests];
+    }
+    [self.tableView reloadData];
+}
+
+- (void)setRequestsFromOthers:(NSMutableArray *)requestsFromOthers
+{
+    _requestsFromOthers = requestsFromOthers;
+    
+    if (![self.allRequests count]) {
+        [self.allRequests addObject:requestsFromOthers];
+    }else if ([self.myRequests count] && [self.allRequests count] == 1){
+        [self.allRequests addObject:requestsFromOthers];
+    }else if ([self.allRequests count] == 2){
+       [self.allRequests replaceObjectAtIndex:1 withObject:requestsFromOthers];
+      //  [self.allRequests insertObject:requestsFromOthers atIndex:0];
+    }else{
+        [self.allRequests replaceObjectAtIndex:0 withObject:requestsFromOthers];
+
+    }
+    
+    [self.tableView reloadData];
+
 }
 
 - (void)viewDidLoad
@@ -36,8 +75,10 @@
 
 - (void)viewWillAppear:(BOOL)animated
 {
+    
     [super viewWillAppear:animated];
-    [self retrieveRequest];
+    [self retrieveMyRequests];
+    [self retrieveRequestsFromOthers];
     [self.navigationController.navigationBar setHidden:NO];
 
     
@@ -49,13 +90,13 @@
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
     // Return the number of sections.
-    return 1;
+    return [self.allRequests count];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     // Return the number of rows in the section.
-    return [self.requests count];
+    return [[self.allRequests objectAtIndex:section] count];
 }
 
 
@@ -64,12 +105,22 @@
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"request" forIndexPath:indexPath];
     
     // Configure the cell...
-    PFObject *request = [self.requests objectAtIndex:indexPath.row];
+    PFObject *requestBook = [[self.allRequests objectAtIndex:indexPath.section] objectAtIndex:indexPath.row];
     //cell.textLabel.text = [NSString stringWithFormat:@"For %@, %@ sent you a request", request[@"title"], request[@""] ];
-    
-    
+    cell.textLabel.text = requestBook[@"title"];
     return cell;
 }
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+   
+    if ([self.allRequests objectAtIndex:section] == self.myRequests){
+        return @"My Requests";
+    }else{
+        return @"Requests from others";
+    }
+    
+}
+
 
 
 
@@ -84,7 +135,7 @@
         // set up the vc to run here
         
         NSIndexPath *indexPath = [self.tableView indexPathForCell:sender];
-        PFObject *myRequestBook = [self.requests objectAtIndex:indexPath.row];
+        PFObject *myRequestBook = [self.allRequests objectAtIndex:indexPath.row];
         
         mrvc.myRequestBook = myRequestBook;
         mrvc.title = [myRequestBook objectForKey:@"title"];
@@ -103,22 +154,21 @@
 
 # pragma mark - Helper methods
 
-- (void)retrieveRequest
+- (void)retrieveMyRequests
 {
     // My requests of others' books
     PFUser *user = [PFUser currentUser];
     PFQuery *query = [PFQuery queryWithClassName:@"Books"];
-    [query whereKey:@"requesterId" equalTo:user.objectId];
     [query orderByDescending:@"updatedAt"];
+    [query whereKey:@"requesterId" equalTo:user.objectId];
     [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
         if (error){
             NSLog(@"Error %@ %@", error, [error userInfo]);
         }else{
-           self.requests = objects;
-            [self.tableView reloadData];
-
+           self.myRequests = [[NSMutableArray alloc] initWithArray:objects];
         }
     }];
+    
 
     
     //May be useful when fetching the request from other people
@@ -150,7 +200,21 @@
     
 }
 
-
+- (void)retrieveRequestsFromOthers
+{
+    PFUser *user = [PFUser currentUser];
+    PFQuery *query = [PFQuery queryWithClassName:@"Books"];
+    [query orderByDescending:@"updatedAt"];
+    [query whereKey:@"holder" equalTo:user.objectId];
+    [query whereKeyExists:@"requesterId"];
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        if (error){
+            NSLog(@"Error %@ %@", error, [error userInfo]);
+        }else{
+            self.requestsFromOthers = [[NSMutableArray alloc] initWithArray:objects];
+        }
+    }];
+}
 
 
 
